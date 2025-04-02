fe1620fb083262764ac1f7d532d960e2bab4473a
Votre Nom <votre.email@exemple.com>
2025-04-02 09:47:54 +0100

    Update GitHub Pages deployment configuration

--- next.config.js
+++ next.config.js
@@ -3,10 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['via.placeholder.com', 'lh3.googleusercontent.com', 'res.cloudinary.com', 'upload.wikimedia.org'],
+    unoptimized: true,
   },
   output: 'export',
   basePath: process.env.NODE_ENV === 'production' ? '/PSP-Social' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/PSP-Social/' : '',
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
